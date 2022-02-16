3f5377aeb55c46224489150b430286ce3249fec8
Zair Abbas <zairabbas1532003@gmail.com>
2022-02-16 18:29:29 +0500

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleshow(true);
+      } else handleshow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/Row.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  // A snippet of code which run based on a specific condition/variable
+
+  useEffect(() => {
+    //if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleclick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleclick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+export default Row;
